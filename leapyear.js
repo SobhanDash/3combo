f2cb4badf37a355cfd3ce2e3d882d9a55520c2d9
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2020-12-24 21:05:40 +0530

    first commit

---
+++ leapyear.js
@@ -0,0 +1,26 @@
+var dob = prompt("Enter your DOB in dd/mm/yyyy format: ");
+alist = dob.split(/[/]/);
+// console.log(alist);
+year = alist[2];
+// console.log(year);
+var leapYearMessage="Your birth year is a leap year!";
+var notLeapYearMessage ="Your birth year is not a leap year!"
+
+function leapYear(year1){
+  if(year1 % 4 === 0){
+    if(year1 % 100 === 0){
+      if(year1 % 400 === 0){
+        return leapYearMessage;
+      }
+      else{
+        return notLeapYearMessage;
+      }
+    }
+    else{
+      return leapYearMessage;
+    }
+  }
+  return notLeapYearMessage;
+}
+
+alert(leapYear(year))
---
+++ palindrome.js
@@ -0,0 +1,21 @@
+var userInput = prompt("Enter a number:");
+
+// split -> reverse -> join
+
+function checkpalin(input){
+    var splitarr = input.split("");
+    
+    var reversearr = splitarr.reverse();
+    
+    var revstr = reversearr.join("");
+    
+
+    if (input===revstr){
+        return "It is a palindrome"
+    }
+    else{
+        return "It's not a palindrome"
+    }
+}
+
+alert(checkpalin(userInput));
